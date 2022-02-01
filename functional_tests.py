8038c6d42b81f94a41ece3c3101b133624048f97
Filipe Vitorino <filipe0vit@gmail.com>
2022-01-31 21:51:12 -0300

    Salvando o POST no Banco de Dados

--- functional_tests.py
+++ functional_tests.py
@@ -52,10 +52,15 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfeita, ela vai dormir
 
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def test_can_save_a_POST_request(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
+
+    self.assertIn('A new list item', response.content.decode())
+    self.assertTemplateUsed(response, 'home.html')
 
 if __name__ == '__main__':
     unittest.main()
