74f84c649dc3dda015fe9ec2e91c19d25d2aaec4
Filipe Vitorino <filipe0vit@gmail.com>
2022-01-31 22:14:48 -0300

    Criando BD de Produção

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self): 
     
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
@@ -52,15 +57,7 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfeita, ela vai dormir
 
 
-    def test_can_save_a_POST_request(self):
-    response = self.client.post('/', data={'item_text': 'A new list item'})
-
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new list item')
-
-    self.assertIn('A new list item', response.content.decode())
-    self.assertTemplateUsed(response, 'home.html')
+   
 
 if __name__ == '__main__':
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -11,23 +11,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
     
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')  
-
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html') 
-        #self.assertTemplateUsed(response, 'wrong.html')
-    
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
